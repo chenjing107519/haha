3b9b2906a15570d63fa714f67381b8db5c28a6b5
chenjing <chenjing@192.168.135.102>
2018-06-30 10:02:29 +0800

    333

---
+++ GithubTest-3/src/main/java/com/example/demo/GithubTest3Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubTest3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubTest3Application.class, args);
+	}
+}
---
+++ GithubTest-4/src/main/java/com/example/demo/GithubTest4Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubTest4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubTest4Application.class, args);
+	}
+}
