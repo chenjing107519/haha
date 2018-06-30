6713652ae1cdc2f4dc8e9454001804b7156f02a2
chenjing <chenjing@192.168.135.102>
2018-06-30 10:07:00 +0800

    666

---
+++ GithubTest-6/src/main/java/com/example/demo/GithubTest6Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubTest6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubTest6Application.class, args);
+	}
+}
